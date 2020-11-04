8bfa38fdac37af166e33ad36004977528b88123d
YungLawn <cteelon@usca.edu>
2020-11-04 13:15:58 -0500

    1 file modified

--- js/add-element.js
+++ js/add-element.js
@@ -7,22 +7,61 @@ $("li.hot").removeClass("hot");
 $("li").addClass("cool");
 
 // traverse the elements
+$("#one").next().next().text("milk");
 
 // add a new element
 
 // add by clicking the plus sign
-document.getElementById("add").addEventListener("click", addElement);
+$('#add').click(addElement);
 
 function addElement() {
-  // add a new element
+  // add a input text box
+  $('#todo').append("<li class='cool'> <input type='text' placeholder='New Item'> </input> </li>");
+
+  //whenever user is document
+  $('input').blur(function(){
+    $(this).parent().addClass('cool');
+
+    //bind click with new li element
+    $(this).parent().click(changeStyle);
+
+    //remove input element and replace with typed value
+    var userinput = $(this).val();
+    $(this).parent().text(userinput);
+
+  });
 }
 
+$("li").click(changeStyle); //bind click with event
+
 //  click the li element will change the changeStyle
-function changeStyle() {}
+function changeStyle() {
+  if($(this).hasClass('complete')){
+    $(this).removeClass('complete');
+    $(this).addClass('cool');
+  }
+  else{
+    //if it is cool class, remove cool and add complete
+    //if it is hot class, remove hot and add complete
+    //else, add complete
+    if($(this).hasClass('cool')){
+      $(this).removeClass('cool');
+      $(this).addClass('complete');
+    }
+    if($(this).hasClass('hot')){
+      $(this).removeClass('hot');
+      $(this).addClass('complete');
+    }
+    else{
+      $(this).addClass('complete');
+    }
+  }
+}
 
 // delete by clicking the trash can
 document.getElementById("remove").addEventListener("click", removeElement);
 
 function removeElement() {
   // remove the marked element
+  $('li.complete').remove();
 }
