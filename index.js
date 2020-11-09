7c919c65867f35aab7a789b4772701b84fe76bb1
YungLawn <cteelon@usca.edu>
2020-11-09 13:03:24 -0500

    1 file modified

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ import * as firebaseui from "firebaseui";
 //  Code for later database connection Please ignore.
 
 // Add Firebase project configuration object here
-var firebaseConfig = {
-
+const firebaseConfig = {
+  apiKey: "AIzaSyA6BlQyU38IZj3MNizq4KeKttMvqnNuftU",
+  authDomain: "grocerylist-41c62.firebaseapp.com",
+  databaseURL: "https://grocerylist-41c62.firebaseio.com",
+  projectId: "grocerylist-41c62",
+  storageBucket: "grocerylist-41c62.appspot.com",
+  messagingSenderId: "899318465752",
+  appId: "1:899318465752:web:bd2ef218b10ac6c33b03ad",
+  measurementId: "G-52120D3SXT"
 };
 
 firebase.initializeApp(firebaseConfig);
 
-
-
 // Save the list to database
-
