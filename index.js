7283d984c151cfd4ea632185a1c2d942eff7d838
YungLawn <cteelon@usca.edu>
2020-11-11 12:35:17 -0500

    1 file modified

--- index.js
+++ index.js
@@ -30,3 +30,10 @@ var firebaseConfig = {
 firebase.initializeApp(firebaseConfig);
 
 // Save the list to database
+
+$("#save").click(function(){
+    firebase.firestore().collection("mylist").add({
+      item: "try it"
+    });
+    console.log("save it");
+});
